16e52b91e1d1517c7213df3ce526decdfd19a267
Ryuugamine <yoerdanatan@gmail.com>
2024-05-06 23:08:34 +0700

    set up supabase

---
+++ app/logout/actions.ts
@@ -0,0 +1,20 @@
+"use server";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
+
+export async function signOutUser() {
+  const supabase = createClient();
+
+  const { error } = await supabase.auth.signOut();
+
+  console.log(error);
+  console.log("here");
+
+  if (error) {
+    redirect("/error");
+  }
+
+  revalidatePath("/", "layout");
+  redirect("/login");
+}
