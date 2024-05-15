737b7f89128ac701c429e2f58e5d3664a43ef834
Ryuugamine <yoerdanatan@gmail.com>
2024-05-16 01:19:41 +0700

    style : add crumbreads

--- app/(dashboard)/dashboard/user/page.tsx
+++ app/(dashboard)/dashboard/user/page.tsx
@@ -5,11 +5,30 @@ import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { Plus } from "lucide-react";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 
 export default async function page() {
   const data = await getAllUser();
   return (
-    <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
+    <div className="flex-1 space-y-4 px-4 md:px-8">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/dashboard">Dashboard</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink className="font-semibold">User</BreadcrumbLink>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
       <div className="flex flex-col space-y-2">
         <h2 className="text-3xl font-bold tracking-tight">Users</h2>
         <p className="tracking-tight text-md text-muted-foreground">
