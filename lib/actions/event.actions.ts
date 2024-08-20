fd25b99244021d2ed9d0352fd796bf2e8bd7df0e
Ryuugamine <yoerdanatan@gmail.com>
2024-08-20 15:28:45 +0700

    chore : change event actions name

---
+++ lib/actions/event.actions.ts
@@ -0,0 +1,32 @@
+"use server";
+
+import { CreateEventParams } from "@/types";
+import { handleError } from "../utils";
+import { connectToDatabase } from "../database";
+import User from "../database/models/user.model";
+import Event from "../database/models/event.model";
+
+export const createEvent = async ({
+  event,
+  userId,
+  path,
+}: CreateEventParams) => {
+  try {
+    await connectToDatabase();
+    const organizer = await User.findById(userId);
+
+    if (!organizer) {
+      throw new Error("Organizer not found ");
+    }
+
+    const newEvent = await Event.create({
+      ...event,
+      category: event.categoryId,
+      organizer: userId,
+    });
+
+    return JSON.parse(JSON.stringify(newEvent));
+  } catch (error) {
+    handleError(error);
+  }
+};
